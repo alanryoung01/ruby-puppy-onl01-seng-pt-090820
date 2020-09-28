8cf11c5200f9b2affbe6ba1e77da638eb455b0bf
Alan Young <alanryoung01@gmail.com>
2020-09-28 04:31:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   attr_accessor :name
-  
+
   @@all = []
 
 def initialize(name)
@@ -9,10 +9,10 @@ def initialize(name)
 end
 
 def self.clear_all
-  @@all.clear 
+  @@all.clear
 end
 
-def self.all 
+def self.all
   @@all.each do |dog|
     puts dog.name
   end
